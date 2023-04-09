39e990106e5ac21a48cb7e462926e0fcb81f4911
Emir <m1r4@yandex.com>
2023-04-09 11:37:36 +0000

    ADD: blackjack iteration

--- exercism/go/blackjack/blackjack.go
+++ exercism/go/blackjack/blackjack.go
@@ -2,57 +2,44 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	switch card {
-	case "ace":
-		return 11
-	case "two":
-		return 2
-	case "three":
-		return 3
-	case "four":
-		return 4
-	case "five":
-		return 5
-	case "six":
-		return 6
-	case "seven":
-		return 7
-	case "eight":
-		return 8
-	case "nine":
-		return 9
-	case "king":
-		fallthrough
-	case "ten":
-		fallthrough
-	case "queen":
-		fallthrough
-	case "jack":
-		return 10
-	default:
-		return 0
+	cards := map[string]int{
+		"ace":   11,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"king":  10,
+		"ten":   10,
+		"queen": 10,
+		"jack":  10,
 	}
 
+	return cards[card]
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	myCards := ParseCard(card1) + ParseCard(card2)
+	myCardsTotal := ParseCard(card1) + ParseCard(card2)
+	dealerCardTotal := ParseCard(dealerCard)
+
 	if card1 == card2 && card1 == "ace" {
 		return "P"
-	} else if myCards == 21 && ParseCard(dealerCard) < 10 {
+	} else if myCardsTotal == 21 && dealerCardTotal < 10 {
 		return "W"
-	} else if myCards == 21 && ParseCard(dealerCard) >= 10 {
+	} else if myCardsTotal == 21 && dealerCardTotal >= 10 {
 		return "S"
-	} else if myCards >= 17 {
+	} else if myCardsTotal >= 17 {
 		return "S"
-	} else if myCards >= 12 && ParseCard(dealerCard) >= 7 {
+	} else if myCardsTotal >= 12 && dealerCardTotal >= 7 {
 		return "H"
-	} else if myCards >= 12 {
+	} else if myCardsTotal >= 12 {
 		return "S"
 	}
 
 	return "H"
-
 }
